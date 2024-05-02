f6e99fd58952b11510414ae9e7f187b22ad791dd
Gianlu03 <johndoe@example.com>
2024-05-02 12:40:26 +0200

    es6

--- eseC/ese3/es5/myCatConFork.c
+++ eseC/ese3/es5/myCatConFork.c
@@ -11,12 +11,14 @@ int main (int argc, char** argv)
     int pid;        			/* per fork */
     int pidFiglio, status, ritorno;     /* per wait padre */
 
-    	/* controlliamo che si passino esattamente due parametri: stringa da cercare nome del file dove cercare! */ 
-	if (argc != 3)
+    /* controlliamo che si passi esattamente un parametro*/
+	if (argc-1 != 1)
 	{
-       		printf("Errore nel numero di parametri che devono essere due (stringa da cercare e nome del file dove cercare): %d\n", argc);
+       		printf("Errore nel numero di parametri che deve essere 1 solo(nome file");
        		exit(1);	
 	}
+
+
 
 	/* generiamo un processo figlio dato che stiamo simulando di essere il processo di shell! */
         if ((pid = fork()) < 0)
@@ -27,22 +29,16 @@ int main (int argc, char** argv)
 
 	if (pid == 0)
 	{ 	/* figlio */
-		printf("Esecuzione di grep da parte del figlio con pid %d\n", getpid());
+		printf("Esecuzione di mycat da parte del figlio con pid %d\n", getpid());
 	/* ridirezionamo lo standard output su /dev/null perche' ci interessa solo se il comando grep ha successo o meno */
-		close(1);
-		open("/dev/null", O_WRONLY);
-        close(2);
-        open("/dev/null", O_WRONLY);
-
 		close(0);
-        if(open(argv[2], O_RDONLY) < 0)
-			exit(-1);
-
-		execlp("grep", "grep", argv[1], (char *)0);
+        if(open(argv[1], O_RDONLY) < 0)
+            exit(-2); /* errore 2 = 254*/
+		execl("/home/gian/Desktop/Operating_Systems/eseC/ese3/es5/mycat", "mycat", (char *)0); //richiamo mycat senza parametri perchè il file è aperto in 0
 
  		/* si esegue l'istruzione seguente SOLO in caso di fallimento della execlp */
 		/* ATTENZIONE SE LA EXEC FALLISCE NON HA SENSO FARE printf("Errore in execlp\n"); DATO CHE LO STANDARD OUTPUT E' RIDIRETTO SU /dev/null */
-        	exit(-1); /* torniamo al padre un -1 che sara' interpretato come 255 e quindi identificato come errore */
+        exit(-1); /* torniamo al padre un -1 che sara' interpretato come 255 e quindi identificato come errore */
 	}
 
 	/* padre aspetta subito il figlio appunto perche' deve simulare la shell e la esecuzione in foreground! */		
@@ -55,12 +51,10 @@ int main (int argc, char** argv)
         	printf("Figlio con pid %d terminato in modo anomalo\n", pidFiglio);
 	else
 	{	
-       		ritorno=(int)((status >> 8) & 0xFF);
-       		printf("Il figlio con pid=%d ha ritornato %d (se 255 problemi!)\n", pidFiglio, ritorno);
+        ritorno=(int)((status >> 8) & 0xFF);
+        printf("Il figlio con pid=%d ha ritornato %d (se 254-5 problemi!)\n", pidFiglio, ritorno);
 		if (ritorno == 0)
-       			printf("Quindi, il figlio %d ha trovato la stringa %s nel file %s\n", pidFiglio, argv[1], argv[2]);
-		else
-       			printf("Quindi, il figlio %d NON ha trovato la stringa %s nel file %s oppure il file %s non esiste\n", pidFiglio, argv[1], argv[2], argv[2]);
+       			printf("il figlio %d ha ritornato %d\n", pidFiglio, ritorno);
 	}	
 	exit(0);
 }
