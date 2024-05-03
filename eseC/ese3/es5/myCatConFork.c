b49addd9e722491c588e558f3778ab0a062f2920
Gianlu03 <johndoe@example.com>
2024-05-03 14:54:46 +0200

    correzioni

--- eseC/ese3/es5/myCatConFork.c
+++ eseC/ese3/es5/myCatConFork.c
@@ -12,16 +12,15 @@ int main (int argc, char** argv)
     int pidFiglio, status, ritorno;     /* per wait padre */
 
     /* controlliamo che si passi esattamente un parametro*/
-	if (argc-1 != 1)
+	int N = argc-1;
+	if (N != 1)
 	{
-       		printf("Errore nel numero di parametri che deve essere 1 solo(nome file");
-       		exit(1);	
+		printf("Errore nel numero di parametri che deve essere 1 solo(nome file), forniti %d\n", N);
+		exit(1);	
 	}
 
-
-
 	/* generiamo un processo figlio dato che stiamo simulando di essere il processo di shell! */
-        if ((pid = fork()) < 0)
+    if ((pid = fork()) < 0)
 	{	/* fork fallita */
 		printf("Errore in fork\n");
 		exit(2);
@@ -32,8 +31,11 @@ int main (int argc, char** argv)
 		printf("Esecuzione di mycat da parte del figlio con pid %d\n", getpid());
 	/* ridirezionamo lo standard output su /dev/null perche' ci interessa solo se il comando grep ha successo o meno */
 		close(0);
-        if(open(argv[1], O_RDONLY) < 0)
-            exit(-2); /* errore 2 = 254*/
+        if(open(argv[1], O_RDONLY) < 0){
+			printf("Errore apertura file di input %s o non esiste \n", argv[1]);
+			exit(-1);
+		}
+        /* errore 2 = 254*/
 		execl("/home/gian/Desktop/Operating_Systems/eseC/ese3/es5/mycat", "mycat", (char *)0); //richiamo mycat senza parametri perchè il file è aperto in 0
 
  		/* si esegue l'istruzione seguente SOLO in caso di fallimento della execlp */
@@ -44,17 +46,17 @@ int main (int argc, char** argv)
 	/* padre aspetta subito il figlio appunto perche' deve simulare la shell e la esecuzione in foreground! */		
 	if ((pidFiglio=wait(&status)) < 0)
 	{
-      		printf("Errore wait\n");
-      		exit(3);
+		printf("Errore wait\n");
+		exit(3);
 	}
 	if ((status & 0xFF) != 0)
-        	printf("Figlio con pid %d terminato in modo anomalo\n", pidFiglio);
+        printf("Figlio con pid %d terminato in modo anomalo\n", pidFiglio);
 	else
 	{	
         ritorno=(int)((status >> 8) & 0xFF);
-        printf("Il figlio con pid=%d ha ritornato %d (se 254-5 problemi!)\n", pidFiglio, ritorno);
+        printf("Il figlio con pid=%d ha ritornato %d (se 255 problemi!)\n", pidFiglio, ritorno);
 		if (ritorno == 0)
-       			printf("il figlio %d ha ritornato %d\n", pidFiglio, ritorno);
+			printf("il figlio %d ha ritornato %d\n", pidFiglio, ritorno);
 	}	
 	exit(0);
 }
